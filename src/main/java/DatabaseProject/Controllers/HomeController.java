51d69c19156137a5f705cab4baa139b3b84916b4
pregister <pregister@constantcontact.com>
2019-09-24 09:10:10 -0400

    Refactored code.

--- src/main/java/DatabaseProject/Controllers/HomeController.java
+++ src/main/java/DatabaseProject/Controllers/HomeController.java
@@ -1,21 +1,57 @@
 package DatabaseProject.Controllers;
 
+import DatabaseProject.Exceptions.BookNotFoundException;
 import DatabaseProject.Models.BookModel;
-import org.springframework.beans.factory.annotation.Value;
+import DatabaseProject.Repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 
 @Controller
 public class HomeController {
-    @Value("${spring.application.name}")
-    String appName;
+    @Autowired
+    private BookRepository bookRepository;
 
-    @GetMapping("/")
-    public String homePage(Model model) {
-        model.addAttribute("appName", appName);
+    @GetMapping()
+    public String formPage(Model model) {
+        model.addAttribute("book", new BookModel());
+        model.addAttribute("bookCount", bookRepository.count());
+        model.addAttribute("books", bookRepository.findAll());
+        return "home";
+    }
+
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@ModelAttribute BookModel book, Model model){
+        bookRepository.save(book);
+        model.addAttribute("book", new BookModel());
+        model.addAttribute("books", bookRepository.findAll());
+        return "home";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String handleDelete(@PathVariable Long id) {
+        delete(id);
+        return "redirect:/home";
+    }
+
+    @DeleteMapping("/form/{id}")
+    public String delete(@PathVariable Long id) {
+        bookRepository.findById(id).orElseThrow(BookNotFoundException::new);
+        bookRepository.deleteById(id);
+        return "home";
+    }
+
+    @PutMapping("/update")
+    public String updateBook(@RequestBody BookModel book, Model model) {
+        bookRepository.findById(book.getId()).orElseThrow(BookNotFoundException::new);
+        bookRepository.save(book);
+        model.addAttribute("book", new BookModel());
+        model.addAttribute("books", bookRepository.findAll());
         return "home";
     }
 }
